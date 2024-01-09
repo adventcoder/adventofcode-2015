9dd4ff177ac75e5c6ea9e92188528e17d0c1a073
adventcoder <59347361+adventcoder@users.noreply.github.com>
2024-01-09 08:55:49 +0000

    int coords

--- day18.py
+++ day18.py
@@ -1,19 +1,26 @@
 from utils import get_input
 from collections import Counter
 
-dirs = [complex(x, y) for y in (-1, 0, 1) for x in (-1, 0, 1) if (x, y) != (0, 0)]
+width = 100
+height = 100
+
+def coord(x, y):
+    return x + y*(width + 2)
+
+def valid(p):
+    x, y = divmod(p, width + 2)
+    return 0 <= x < width and 0 <= y < height
+
+dirs = [coord(x, y) for y in (-1, 0, 1) for x in (-1, 0, 1) if (x, y) != (0, 0)]
 
 def parse_state(s):
-    return set(complex(x, y) for y, line in enumerate(s.splitlines()) for x, c in enumerate(line) if c == '#')
+    return set(coord(x, y) for y, line in enumerate(s.splitlines()) for x, c in enumerate(line) if c == '#')
 
 def update(state):
     counts = Counter(p + d for p in state for d in dirs)
     return set(p for p, count in counts.items() if (p in state and 2 <= count <= 3) or (p not in state and count == 3 and valid(p)))
 
-def valid(p):
-    return 0 <= p.real < 100 and 0 <= p.imag < 100
-
-corners = [0, 99, 99j, 99+99j]
+corners = [coord(0, 0), coord(width - 1, 0), coord(0, height - 1), coord(width - 1, height - 1)]
 state1 = parse_state(get_input(18))
 state2 = state1.copy()
 state2.update(corners)
